7ed23852e2d50621baa3b30caf1091e2d9cb4d5d
aisomura <aisomura@fout.jp>
2023-06-02 15:10:11 +0900

    feat: can notify slack

---
+++ index.js
@@ -0,0 +1,45 @@
+const { IncomingWebhook } = require('@slack/webhook');
+
+class ServerlessDeployNotifier {
+  constructor(serverless, options) {
+    this.serverless = serverless;
+    this.options = options;
+
+    this.hooks = {
+      'before:deploy:deploy': this.beforeDeployTask.bind(this),
+      'after:deploy:deploy': this.afterDeployTask.bind(this),
+    };
+  }
+
+  beforeDeployTask() {
+    const functionName = this.serverless.service.serviceObject.name;
+    const text = `[Lambda] "${functionName}" deployment started.`
+    this.postSlack(text, "warning")
+  }
+  afterDeployTask() {
+    const functionName = this.serverless.service.serviceObject.name;
+    const text = `[Lambda] "${functionName}" deployment finished.`
+    this.postSlack(text, "good")
+  }
+  postSlack(text, color) {
+    const environment = this.serverless.service.custom.environment
+    const message = {
+      text: environment.TEXT,
+      channel: environment.CHANNEL,
+      username: environment.USER_NAME,
+      icon_emoji: environment.ICON_EMOJI,
+      color: color,
+    };
+    const webhook = new IncomingWebhook(environment.SLACK_URL);
+
+    webhook.send(message)
+      .then(() => {
+        console.log('Message sent!');
+      })
+      .catch((error) => {
+        console.error('Error sending message:', error);
+      });
+  }
+}
+
+module.exports = ServerlessDeployNotifier;
